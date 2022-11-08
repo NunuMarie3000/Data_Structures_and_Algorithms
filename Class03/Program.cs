c5548ec3c8e0ae462cee343d0757adbe76a4a5f4
Storm O'Bryant <vmarie1997@gmail.com>
2022-11-08 00:29:09 -0600

    restructuring

--- Class03/Program.cs
+++ Class03/Program.cs
@@ -5,34 +5,59 @@
     static void Main(string[] args)
     {
       int searchKey = 15;
-      int[] sortedArr = {4, 8, 15, 16, 23, 42};
+      int[] sortedArr = { 4, 8, 15, 16, 23, 42 };
 
-      BinarySearch(sortedArr, searchKey);
+      Console.WriteLine(BinarySearchDisplay(sortedArr, searchKey));
+      // BinarySearch(sortedArr, searchKey);
       // Console.WriteLine(Array.BinarySearch(sortedArr, searchKey));
 
     }
 
-    static void BinarySearch(int[] sortedArr, int searchKey)
+    public static object BinarySearchDisplay(int[] arr, int key)
     {
-      
+      int minNum = 0;
+      int maxNum = arr.Length - 1;
 
-
-      // List<int> sortedArrList = new List<int>();
-      // foreach(int i in sortedArr)
-      //   sortedArrList.Add(i);
-      
-      // int whatToReturn;
-      // int middleIndex = (sortedArrList.Count() - 1) / 2; 
-      // while(whatToReturn == null)
-      // {
-      //   if(sortedArrList.ElementAt(middleIndex) == searchKey)
-      //     whatToReturn = middleIndex;
-      //   else
-      //   {
-      //     // other operation here
-
-      //   }
-      // }
+      while (minNum <= maxNum)
+      {
+        int mid = (minNum + maxNum) / 2;
+        if (key == arr[mid])
+        {
+          return ++mid;
+        }
+        else if (key < arr[mid])
+        {
+          maxNum = mid - 1;
+        }
+        else
+        {
+          minNum = mid + 1;
+        }
+      }
+      return "None";
     }
+
+    // static void BinarySearch(int[] sortedArr, int searchKey)
+    // {
+
+
+
+    //   List<int> sortedArrList = new List<int>();
+    //   foreach(int i in sortedArr)
+    //     sortedArrList.Add(i);
+
+    //   int whatToReturn;
+    //   int middleIndex = (sortedArrList.Count() - 1) / 2; 
+    //   while(whatToReturn == null)
+    //   {
+    //     if(sortedArrList.ElementAt(middleIndex) == searchKey)
+    //       whatToReturn = middleIndex;
+    //     else
+    //     {
+    //       // other operation here
+
+    //     }
+    //   }
+    // }
   }
 }
