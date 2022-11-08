2fcf9141ebf623cd727f16bf4acfa7f48b10c9a5
Storm O'Bryant <vmarie1997@gmail.com>
2022-11-08 06:57:53 -0600

    class04

--- Class04/Program.cs
+++ Class04/Program.cs
@@ -4,32 +4,62 @@
   {
     static void Main(string[] args)
     {
-      QuestionOne();
-      // QuestionTwo();
+      int[,] myArr = {{1, 2, 3}, {3, 5, 7}, {1, 7, 10}};
+      int[,] anotherArr = {{0, 1, 5}, {-4, 7, 2}, {-3, 12, 11}};
+      // looks like
+        // {1, 2, 3}
+        // {3, 5, 7}
+        // {1, 7, 10}
+      QuestionOne(anotherArr);
+      // QuestionOne(myArr);
+      // Console.WriteLine(QuestionTwo(7));
     }
 
-    public void QuestionOne()
+    public static void QuestionOne(int[,] arr)
     {
       // Ask the candidate to write a function to add up the sum of each row in a matrix of arbitrary size, and return an array with the appropriate values.
       // Avoid utilizing any of the built-in methods available to your language.
-      // Don’t let the candidate get scared by the term “matrix”… It’s just an array of arrays.
-      // The matrix will always be full of integers.
       // Negative values are possible.
       // All nulls will be counted as zeros.
+      // Console.WriteLine(arr.GetLength(0));
+      int numberOfRows = arr.GetLength(0);
+      int itemsInRow = arr.GetLength(1);
+      int totalSum = 0;
+      for(int i = 0; i <= numberOfRows - 1; i++) // for each row in the input array
+      {
+        for(int j = 0; j <= itemsInRow - 1; j++) // for each item in the row
+        // Console.Write(arr[i, j] + " ");
+        totalSum += arr[i, j];
+      }
+      Console.WriteLine(totalSum);
+      // Console.WriteLine("[ " + numberOfRows + ", " + itemsInRow + " ]"); // these are the dimensions of the input array
     }
 
-    public void QuestionTwo()
+    /*public static int QuestionTwo(int n)
     {
-      // As an interviewer, familiarize yourself with the Fibonacci Sequence
-      //Ask the candidate to write a function to accepts an integer, and returns the nth number in the Fibonacci sequence.
+      // return nth number in the Fibonacci sequence.
       // You should be able to check their work for small values of n: if fib(n) is equal to fib(n-1) + fib(n-2).
-      // Encourage the candidate to work quickly towards a first solution, that is either recursive or iterative (with a while or for loop).
-      // Then ask the candidate to solve it with the other approach (eg: if they first used an iterative solution, ask for a recursive solution)
-      // Evaluate and compare the Big-O of both solutions.
-      // The recursive solution might be as bad as O(2n)—that’s 2 to the power of n— which is so bad most laptops would take a while to solve for n larger than about 40.
-      // The iterative solution should be roughly O(n), which means a laptop could find answers for large values of n.
-      // There is also an O(1) solution, based on a mathematical formula… Not likely anyone will know this without looking it up! (Did you see the formula on the page about the sequence linked above?) If time allows, try to implement the formula with the candidate.
-      // Avoid utilizing any of the built-in methods available to your language.
-    }
+      // counter is what's gonna keep up with the nth fib we're on
+
+      // =============================== Recursive ===============================
+      // if(n == 0) return 0;
+      // if(n == 1) return 1;
+      // return QuestionTwo(n - 1) + QuestionTwo(n - 2);
+      // =============================== For Loop ================================
+      // int first = 0;
+      // int second = 1;
+      // int current = 0;
+
+      // if (n == first) return first;
+      // if (n == second) return second;
+
+      // for (int i = 2; i <= n; i++)
+      // {
+      //   current = first + second;
+      //   first = second;
+      //   second = current;
+      // }
+      // return current;
+    }*/
   }
 }
